626f29c904c48d91bdab5c7b30363579513217a7
omarikas <86628361+omarikas@users.noreply.github.com>
2025-01-01 15:21:17 +0200

    added song

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.center').addEventListener('click', function() {
         this.style.display = 'none';
         document.querySelector('.rotating-text').style.display="block"
+
+        document.querySelector('#i2025').style.display="block"
     });
 });
